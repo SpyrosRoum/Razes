8efb5e4ba4f01ab74ff479e01f82e993a47cfea6
Spyros Roum <spyrosr1@gmail.com>
2020-06-02 12:44:17 +0300

    Parse arguments in main and call solver or generator

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,14 @@ fn main() {
     let args = Args::collect();
 
     match args.cmd {
-        Command::Gen {width, height, output_file} => generator::generate(width, height, output_file, args.time_it),
-        Command::Solve {input_file, output_file} => solver::solve(input_file, output_file, args.time_it)
+        Command::Gen {
+            width,
+            height,
+            output_file,
+        } => generator::generate(width, height, output_file, args.time_it),
+        Command::Solve {
+            input_file,
+            output_file,
+        } => solver::solve(input_file, output_file, args.time_it),
     }
 }
