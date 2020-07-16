432f51214da0fb874e1bd531957d4cd3f8cb625d
Spyros Roum <spyrosr1@gmail.com>
2020-07-16 17:04:01 +0300

    Implement base of Solver

--- src/solver.rs
+++ src/solver.rs
@@ -1,1 +1,16 @@
-pub fn solve(in_file: String, of: String, time_it: bool) {}
+use image::{ImageBuffer, ImageFormat, Rgb, RgbImage, self};
+
+pub struct Solver {
+    in_file: String,
+}
+
+impl Solver {
+    pub fn new(in_file: String) -> Self {
+        Solver {
+            in_file,
+        }
+    }
+
+    pub fn solve(&self) -> RgbImage {
+    }
+}
