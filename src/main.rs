22c85c2d4a9e65e48498ea34a9265a0280df73fb
Spyros Roum <spyrosr1@gmail.com>
2020-07-16 17:03:02 +0300

    Refactor main.rs

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,22 @@ use args::{Args, Command};
 use std::time::Instant;
 
 use generator::Generator;
+use solver::Solver;
+use image::RgbImage;
+
+
+fn time_it<T, F: FnMut(T) -> RgbImage>(fun: F, for_: T) -> RgbImage {
+    let start_time = Instant::now();
+
+    let img = fun(for_);
+
+    println!(
+        "It took {} seconds.",
+        start_time.elapsed().as_secs_f64()
+    );
+
+    img
+}
 
 fn main() {
     let args = Args::collect();
@@ -16,23 +32,32 @@ fn main() {
             height,
             output_file,
         } => {
-            let start_time = Instant::now();
-
-            // , output_file, args.time_it
             let mut gen = Generator::new(width as usize, height as usize);
-            let img = gen.generate();
-            if args.time_it {
-                println!(
-                    "It took {} seconds to generate the maze.",
-                    start_time.elapsed().as_secs_f64()
-                );
-            }
+
+            let img = if args.time_it {
+                    time_it(Generator::generate, &mut gen)
+            } else {
+                gen.generate()
+            };
+
+
             println!("Saving as {}..", output_file);
             img.save(output_file).expect("Error saving the maze.");
         }
         Command::Solve {
             input_file,
             output_file,
-        } => solver::solve(input_file, output_file, args.time_it),
+        } => {
+            let solver = Solver::new(input_file);
+
+            let img = if args.time_it {
+                time_it(Solver::solve, solver)
+            } else {
+                solver.solve()
+            };
+
+            println!("Saving as {}..", output_file);
+            img.save(output_file).expect("Error saving the maze.");
+        }
     }
 }
